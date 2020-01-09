0e0ab779d63a3aea0e5683678f2082ed99d62a84
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-09 11:22:34 -0500

    update

---
+++ first_progam/first_progam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  first_progam
+//
+//  Created by Keerthana Madhavan on 1/9/20.
+//  Copyright © 2020 Keerthana Madhavan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
