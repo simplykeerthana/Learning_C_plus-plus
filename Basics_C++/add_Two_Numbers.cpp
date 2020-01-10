80c774a9b6add15b93063d331309af10c817dae1
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-10 10:55:44 -0500

    update

---
+++ Basics_C++/add_Two_Numbers.cpp
@@ -0,0 +1,19 @@
+// this program adds the two integers and stores it in a variable
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a,b,c;
+
+    cout<<"Please enter to numbers for addition: ";
+    cin >> a >>b;
+    c = a + b;
+
+    cout<<"The result of a + b is  : " << c;
+    
+    return 0;
+}
