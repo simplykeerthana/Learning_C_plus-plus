fcf6f3be2a948b8a3d07f8ba9f9893933241528b
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-01-10 20:26:11 -0500

    update

---
+++ Basics_C++/compound_assignment.cpp
@@ -0,0 +1,18 @@
+// a program that tests the feautures of c++
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    int sum = 10;
+    int x=5;
+
+    //sum = sum + x;
+    sum +=1;
+    cout<<sum;
+    return 0;
+}
---
+++ Basics_C++/inc_dec.cpp
@@ -0,0 +1,24 @@
+// increment and decrement operators in c++
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int i = 5;
+    cout<<i;
+    //i = i = 1;
+    i++;
+    cout << i;
+    int y = 6;
+    cout << y;
+    y--;
+    cout << y;
+    
+    
+    return 0;
+
+    //the smae thing goes for *, /, %
+}
