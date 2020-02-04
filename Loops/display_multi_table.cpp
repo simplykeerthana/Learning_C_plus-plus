ad9573234017f5ad24d767b61c573cb786b2b150
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-02-04 09:01:35 -0500

    update

---
+++ Loops/display_multi_table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n, i;
+    
+    cout << "Enter n: ";
+    cin >>n;
+    
+    for(int i = 1; i<=10; i++)
+    {
+        cout << n << " x " << i << " = " << i*n << endl;
+        
+    }
+
+    return 0;
+}
