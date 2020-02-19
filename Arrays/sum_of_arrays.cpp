2bc80713e0433fe8ed3eefec1436dab3f565a9f6
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-02-19 13:54:29 -0500

    Create sum_of_arrays.cpp

---
+++ Arrays/sum_of_arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int A[7] = {2,3,4,5,6,7,8};
+    int i, sum = 0;
+    
+    for(i = 0, i < 7; i++)
+    {
+        sum = sum +A[i];
+    }
+    
+    cout << sum << endl;
+    
+    
+    return 0;
+}
