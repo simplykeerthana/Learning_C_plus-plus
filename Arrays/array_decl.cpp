ae77a1e20dae0ceab13fdfdde82b09a405557f2d
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-02-19 13:34:41 -0500

    update

---
+++ Arrays/array_decl.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int A[] = {2, 4, 6, 8, 10, 12};
+    
+    for(int i = 0; i < 6; i++)
+    {
+        cout << A[i] << endl;
+    }
+    
+    
+    
+    return 0;
+}
