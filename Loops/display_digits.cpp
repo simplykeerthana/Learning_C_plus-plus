faa8f5712996dcb0cc406f11fab7f573907db4e7
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-02-19 12:46:54 -0500

    update loops notes

---
+++ Loops/display_digits.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int r, n;
+    
+    cout << "enter n";
+    cin >> n;
+    
+    while (n >0)
+    {
+        
+        r = n % 10;
+        n = n/10;
+        cout << r << endl;
+    }
+    
+    
+    
+    return 0;
+}
