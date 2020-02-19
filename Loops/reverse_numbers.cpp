f849cbb93ce2f89c91fa8e692cd1eb7488e5b3c4
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2020-02-19 13:00:32 -0500

    update reverse.cpp

---
+++ Loops/reverse_numbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n, r, rev = 0, m;
+
+    cout << "Enterr n" ;
+
+    cin >> n;
+    m = n;
+
+while(n>0)
+{
+    r = n %10;
+    n = n/10;
+    rev = rev * 10 + r;
+
+
+}
+    cout << rev<< endl;
+
+
+    return 0;
+
+}
